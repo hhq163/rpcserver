9c99ffed1d8f0c4ab92ac20c5ea7cfc974273300
hhq163 <hhq163@gmail.com>
2019-03-29 18:11:10 +0800

    增加调试日志

--- main.go
+++ main.go
@@ -17,14 +17,15 @@ func main() {
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
+	log.Fatalf("start 0000")
 	s := grpc.NewServer()
 	protocol.RegisterUserServer(s, &User{})
 	if err := s.Serve(lis); err != nil {
 		log.Fatalf("failed to serve: %v", err)
 	}
-
+	log.Fatalf("start 1111")
 	//初始化数据库连接池
 	InitDB()
 	StartWorkers()
-
+	log.Fatalf("start 2222")
 }
